a15132b881ae7c71d1f9f799d36c114a9034d110
katopz <katopz@gmail.com>
2020-05-01 22:03:57 +0700

    Use y=ax+b

--- src/App.js
+++ src/App.js
@@ -1,91 +1,90 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 // adjust training set size
 
-const M = 10;
+const M = 10
+const SIZE = 100
+const ERROR = 10
+const A = 1.5
+const B = 3
 
 // generate random training set
 
-const DATA = [];
+const DATA = []
 
-const getRandomIntFromInterval = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
-
-const createRandomPortlandHouse = () => ({
-  squareMeter: getRandomIntFromInterval(0, 100),
-  price: getRandomIntFromInterval(0, 100),
-});
+const createRandomPortlandHouse = step => ({
+  squareMeter: step * 10 + Math.max(0, ERROR * Math.random() - ERROR * Math.random()),
+  price: Math.max(0, A * step * 10 + ERROR * Math.random() - ERROR * Math.random() + B)
+})
 
 for (let i = 0; i < M; i++) {
-  DATA.push(createRandomPortlandHouse());
+  DATA.push(createRandomPortlandHouse(i))
 }
+console.log(DATA)
 
-const x = DATA.map(date => date.squareMeter);
-const y = DATA.map(date => date.price);
+const x = DATA.map(date => date.squareMeter)
+const y = DATA.map(date => date.price)
 
 // linear regression and gradient descent
 
-const LEARNING_RATE = 0.0003;
+const LEARNING_RATE = 0.0003
 
-let thetaOne = 0;
-let thetaZero = 0;
+let thetaOne = 0
+let thetaZero = 0
 
-const hypothesis = x => thetaZero + thetaOne * x;
+const hypothesis = x => thetaZero + thetaOne * x
 
-const learn = (alpha) => {
-  let thetaZeroSum = 0;
-  let thetaOneSum = 0;
+const learn = alpha => {
+  let thetaZeroSum = 0
+  let thetaOneSum = 0
 
   for (let i = 0; i < M; i++) {
-    thetaZeroSum += hypothesis(x[i]) - y[i];
-    thetaOneSum += (hypothesis(x[i]) - y[i]) * x[i];
+    thetaZeroSum += hypothesis(x[i]) - y[i]
+    thetaOneSum += (hypothesis(x[i]) - y[i]) * x[i]
   }
 
-  thetaZero = thetaZero - (alpha / M) * thetaZeroSum;
-  thetaOne = thetaOne - (alpha / M) * thetaOneSum;
+  thetaZero = thetaZero - (alpha / M) * thetaZeroSum
+  thetaOne = thetaOne - (alpha / M) * thetaOneSum
 }
 
 const cost = () => {
-  let sum = 0;
+  let sum = 0
 
   for (let i = 0; i < M; i++) {
-    sum += Math.pow(hypothesis(x[i]) - y[i], 2);
+    sum += Math.pow(hypothesis(x[i]) - y[i], 2)
   }
 
-  return sum / (2 * M);
+  return sum / (2 * M)
 }
 
 // count iterations
 
-let iteration = 0;
+let iteration = 0
 
 // view
 
 class App extends React.Component {
   componentDidMount() {
-    this.interval = setInterval(this.onLearn, 1);
+    this.interval = setInterval(this.onLearn, 1)
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
+    clearInterval(this.interval)
   }
 
   onLearn = () => {
-    learn(LEARNING_RATE);
+    learn(LEARNING_RATE)
 
-    iteration++;
+    iteration++
 
-    this.forceUpdate();
+    this.forceUpdate()
   }
 
   render() {
     return (
       <div>
-        <Plot
-          x={'squareMeter'}
-          y={'price'}
-        />
+        <Plot x={'squareMeter'} y={'price'} />
 
         <div>
           <Iteration iteration={iteration} />
@@ -93,41 +92,36 @@ class App extends React.Component {
           <Cost />
         </div>
       </div>
-    );
+    )
   }
 }
 
-const Plot = ({ x, y }) =>
-  <svg width="100" height="100">
-    <line
-      x1="0"
-      y1={100 - hypothesis(0)}
-      x2="100"
-      y2={100 - hypothesis(100)}
-    />
+const Plot = ({ x, y }) => (
+  <svg width={SIZE} height={SIZE}>
+    <line x1="0" y1={SIZE - hypothesis(0)} x2={SIZE} y2={SIZE - hypothesis(SIZE)} />
 
-    {DATA.map((date, key) =>
-      <circle
-        key={key}
-        cx={date[x]}
-        cy={100 - date[y]}
-      />
-    )}
+    {DATA.map((date, key) => (
+      <circle key={key} cx={date[x]} cy={SIZE - date[y]} />
+    ))}
   </svg>
+)
 
-const Iteration = ({ iteration }) =>
+const Iteration = ({ iteration }) => (
   <p>
     <strong>Iteration:</strong> {iteration}
   </p>
+)
 
-const Hypothesis = () =>
+const Hypothesis = () => (
   <p>
     <strong>Hypothesis:</strong> f(x) = {thetaZero.toFixed(2)} + {thetaOne.toFixed(2)}x
   </p>
+)
 
-const Cost = () =>
+const Cost = () => (
   <p>
     <strong>Cost:</strong> {cost().toFixed(2)}
   </p>
+)
 
-export default App;
+export default App
